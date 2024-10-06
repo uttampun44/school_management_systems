0046c21c3c40000318e4bc6fb6cb1aced09fbc36
uttampun <uttampun50@gmail.com>
2024-10-06 21:23:49 +0545

    permission middleware

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,16 +15,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+       $userAdmin = User::create([
             'name' => 'Super Admin',
             'email' => 'superadmin@gmail.com',
             'password' => Hash::make('superadmin@123')
         ]);
 
-        User::create([
+        UserRole::create([
+          'role_id' => 1,
+          'user_id' => $userAdmin->id  
+        ]);
+
+       $user = User::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin@123')
         ]);
+
+        UserRole::create([
+            'role_id' => 2,
+            'user_id' => $user->id  
+          ]);
+  
     }
 }

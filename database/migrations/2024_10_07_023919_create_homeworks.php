e0a6f90d222df7c2f27707e15ca105513ed33905
uttampun <uttampun50@gmail.com>
2024-10-07 08:35:17 +0545

    creating homework and events

---
+++ database/migrations/2024_10_07_023919_create_homeworks.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('homeworks', function (Blueprint $table) {
+            $table->id();
+            $table->string('topic');
+            $table->string('homework_image')->nullable();
+            $table->foreignId('subject_id')->constrained('subjects')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('class_id')->constrained('classrooms')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('section_id')->constrained('classroom_sections')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('teacher_id')->constrained('teacher_details')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('homeworks');
+    }
+};
---
+++ database/migrations/2024_10_07_024610_create_school_events.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('school_events', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name');
+            $table->string('event_image')->nullable();
+            $table->string('event_start_date');
+            $table->string('event_expirty_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('school_events');
+    }
+};
